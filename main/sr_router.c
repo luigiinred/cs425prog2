35613c6157f1ea4c79c3030845a0bd1583906d52
Timmy Garrabrant <timmygarrabrant@Timmys-MacBook-Air.local>
2014-03-19 11:16:02 -0700

    Testing packet message on lec

--- main/sr_router.c
+++ main/sr_router.c
@@ -1,10 +1,10 @@
 /**********************************************************************
- * file:  sr_router.c 
- * date:  Mon Feb 18 12:50:42 PST 2002  
- * Contact: casado@stanford.edu 
+ * file:  sr_router.c
+ * date:  Mon Feb 18 12:50:42 PST 2002
+ * Contact: casado@stanford.edu
  *
  * Description:
- * 
+ *
  * This file contains all the functions that interact directly
  * with the routing table, as well as the main entry method
  * for routing. 11
@@ -20,15 +20,15 @@
 #include "sr_router.h"
 #include "sr_protocol.h"
 
-/*--------------------------------------------------------------------- 
+/*---------------------------------------------------------------------
  * Method: sr_init(void)
  * Scope:  Global
  *
  * Initialize the routing subsystem
- * 
+ *
  *---------------------------------------------------------------------*/
 
-void sr_init(struct sr_instance* sr) 
+void sr_init(struct sr_instance* sr)
 {
     /* REQUIRES */
     assert(sr);
@@ -55,7 +55,7 @@ void sr_init(struct sr_instance* sr)
  *
  *---------------------------------------------------------------------*/
 
-void sr_handlepacket(struct sr_instance* sr, 
+void sr_handlepacket(struct sr_instance* sr,
         uint8_t * packet/* lent */,
         unsigned int len,
         char* interface/* lent */)
@@ -66,11 +66,12 @@ void sr_handlepacket(struct sr_instance* sr,
     assert(interface);
 
     printf("*** -> Received packet of length %d \n",len);
+    printf("%s\n", interface);
 
 }/* end sr_ForwardPacket */
 
 
-/*--------------------------------------------------------------------- 
+/*---------------------------------------------------------------------
  * Method:
  *
  *---------------------------------------------------------------------*/
